f1880f6458e5b55d8fdba6a2a760dba9a63c4fbf
evank0301 <111015362+evank0301@users.noreply.github.com>
2022-12-12 22:11:09 -0600

    Added a few more plots

--- FinalProjectCode.R
+++ FinalProjectCode.R
@@ -8,7 +8,7 @@ library(pROC)
 library(randomForest)
 
 #Read the data for our project into R so we can examine the columns
-fp = read.csv("pbp-2021.csv", stringsAsFactors = TRUE)
+fp = read.csv("Data/pbp-2021.csv", stringsAsFactors = TRUE)
 str(fp)
 
 
@@ -54,8 +54,8 @@ fp_cleaned$RushDirection[fp_cleaned$RushDirection == ""] = "NORUSH"
 table(fp_cleaned$RushDirection)
 #Order the factors for RushDirection based on the Offensive line
 fp_cleaned$RushDirection = factor(fp_cleaned$RushDirection,
-                                  levels = c("NORUSH", "LEFT END", "LEFT TACKLE", "LEFT GUARD","CENTER", "RIGHT GUARD", "RIGHT TACKLE", "RIGHT END"), 
-                                  labels = c( "No Rush", "Left End", "Left Tackle", "Left Guard", "Center", "Right Guard", "Right Tackle", "Right End"))
+                            levels = c("NORUSH", "LEFT END", "LEFT TACKLE", "LEFT GUARD","CENTER", "RIGHT GUARD", "RIGHT TACKLE", "RIGHT END"), 
+                            labels = c( "No Rush", "Left End", "Left Tackle", "Left Guard", "Center", "Right Guard", "Right Tackle", "Right End"))
 table(fp_cleaned$RushDirection)
 
 #Modify blanks in pass type to NOPASS
@@ -102,11 +102,11 @@ fp_cleaned$PlayType = as.character(fp_cleaned$PlayType)
 fp_cleaned = subset(fp_cleaned, PlayType == c("RUSH", "PASS", "SCRAMBLE", "TWO-POINT CONVERSION"))
 table(fp_cleaned$PlayType)
 fp_cleaned$PlayType = factor(fp_cleaned$PlayType,
-                             levels = c("PASS", "RUSH", "SCRAMBLE", "TWO-POINT CONVERSION"),
-                             labels = c("Pass", "Rush", "Scramble", "Two Point Conversion"))
+                                levels = c("PASS", "RUSH", "SCRAMBLE", "TWO-POINT CONVERSION"),
+                                labels = c("Pass", "Rush", "Scramble", "Two Point Conversion"))
 
 
-#Exploratory Plots For Rush and Pass Type (Evan)----
+#Exploratory Plots For Rush and Pass Type----
 ggplot(data = fp_cleaned) +
   geom_bar(aes(x = PlayType))
 
@@ -115,120 +115,44 @@ ggplot(data = fp_cleaned) +
 fp_cleaned %>%
   filter(RushDirection != "No Rush") %>%
   ggplot(aes(x = RushDirection)) +
-  geom_bar()
+  geom_bar(fill = "#99d8c9") + 
+  ggtitle("Distribution of Rushing Directions") + 
+  labs(x = "Rush Direction", y = "Number of Plays")
 
 #Examine the above plot again, but fill based on the play scoring in the endzone
 fp_cleaned %>%
   filter(RushDirection != "No Rush") %>%
   ggplot(aes(x = RushDirection, fill = EndzoneScore)) +
   labs(x = "Rush Direction", y = "Frequency") +
-  ggtitle("Distribution of Rush Directions Accross the Offensive Line") + 
-  scale_fill_grey("Outcome of \nthe Play") +
-  geom_bar()
+  ggtitle("Frequencies of Rushers Reaching the Endzone") + 
+  scale_fill_brewer("Outcome of \nthe Play", palette = "BuGn") +
+  geom_bar(position = "fill")
 
 
 #Get a feel for passing frequencies
 fp_cleaned %>%
   filter(PassType != "No Pass") %>%
   ggplot(aes(x = PassType)) + 
-  geom_bar()
+  geom_bar(fill = "#99d8c9") +
+  ggtitle("Distribution of Pass Types") + 
+  labs(x = "Type of Pass", y = "Number of Plays")
 
 #Group by scoring again
 fp_cleaned %>%
   filter(PassType != "No Pass") %>%
-  ggplot(aes(x = PassType, fill = EndzoneScore), position = "fill") + 
-  labs(x = "Pass Type", y = "Frequency") +
-  ggtitle("Distribution of Pass Types on Passing Plays") +
-  scale_fill_grey("Outcome of \nthe Play") +
-  geom_bar()
-
-#Exploratory Plots 
-
-#Plots with Yards
-#Scores based on how many yards the play gained
-ggplot(data = fp_cleaned)+
-  geom_histogram(aes(x=Yards, fill = EndzoneScore), position = "fill", binwidth = 5)+
-  labs(x = "Yards Gained on the Play", y = "Play scored")+
-  ggtitle("Endzone scores on how long the play was")+
-  scale_fill_brewer("Scored", palette = "YlOrRd")
-#Most interesting thing on this histogram is that there are plays that scored
-#touchdowns with negative yards gained on the play. Is this counting defensive touchdwons?
-#if that is the case then a play of more than -20 yards would have happened
-
-#plot of yards gained over pass play type
-ggplot(data = fp_cleaned)+
-  geom_histogram(aes(x=Yards, fill = PassType), position = "fill", binwidth = 5)+
-  labs(x = "Yards Gained on Pass Plays", y = "Pass Type")+
-  ggtitle("Yards gained based on Pass play type")+
-  scale_fill_brewer("Pass Type", palette = "YlOrRd")
-#Of all the deep pass pays, it seems like deep right is the most effective
-#in some areas deep sleep also seems to work well. Ironically from about 77-85
-#yard lines, short left passes seem oddly effective. Although that may be due to a 
-#small sample size and the only pass play(s) to score from around 100 yards is 
-#short right
-
-#plot of yards gained over rush direction 
-ggplot(data = fp_cleaned)+
-  geom_histogram(aes(x=Yards, fill = RushDirection), position = "fill", binwidth = 5)+
-  labs(x = "Yards Gained", y = "Rush Direction")+
-  ggtitle("Yards gained based on Rush Direction")+
-  scale_fill_brewer("Rush \nDirection", palette = "YlOrRd")
-#It seems that the majority of plays for each amount of yards gained are not rushes
-#at all. It also seems the majority of rush plays are about 3-5 yards.The longest
-#runs (55+ seem to come from rushes following the left side)
-
-#PlayType
-#Score by play type
-ggplot(data = fp_cleaned)+
-  geom_bar(aes(x=PlayType, fill = EndzoneScore), position = "fill")+
-  labs(x = "Type of Play", y = "Play scored")+
-  coord_flip()+
-  ggtitle("Endzone scores based on Play Type")+
-  scale_fill_brewer("Scored", palette = "YlOrRd")
-#Two-point conversion are nearly 50% successful in getting into the end zone,
-#Of all pass and rush plays, about 5% truly end in a score. Exception and *blank*
-#both have interestily high score percentages, would like to maybe figure out what that
-#is about
+  ggplot(aes(x = PassType, fill = EndzoneScore)) + 
+  labs(x = "Type of Pass", y = "Frequency") +
+  ggtitle("Frequencies of Scoring on Passing Plays") +
+  scale_fill_brewer("Outcome of \nthe Play", palette = "BuGn") +
+  geom_bar(position = "fill")
 
 
-#YardLine
-#Score by yard line
-ggplot(data = fp_cleaned)+
-  geom_histogram(aes(x=YardLine, fill = EndzoneScore), position = "fill", binwidth = 5)+
-  labs(x = "Yard Line Play Started On", y = "Play scored")+
-  ggtitle("Endzone Scores Based on Where the Play Started")+
-  scale_fill_brewer("Scored", palette = "YlOrRd")
-#The interesting aspect of this graph is that a large portion of plays run from
-#inside about the 10 yard line score. There is also an interesting increase at about the 
-#20 yard line compared to about the 15 yard line, the trend almost seems exponential
 
-#Time
-#Score by time during the game
-ggplot(data = fp_cleaned)+
-  geom_histogram(aes(x=TimeOfPlay, fill = EndzoneScore), position = "fill", binwidth = 5)+
-  labs(x = "Time of Play", y = "Play scored")+
-  ggtitle("Endzone Scores Based on When the Play was Ran")+
-  scale_fill_brewer("Scored", palette = "YlOrRd")
-#There does not seem to be a certain time of the game that allows for more scores
-#except for the very end of overtime where teams my be trying to just tie the game instead
-#of winning it
-
-#Plot with Score
-Score <- ggplot(fp_cleaned, aes(x = EndzoneScore)) +
-  geom_bar()
-Score
-
-#Plot with formation 
-#Score by Formation Type 
-ggplot(data = fp_cleaned)+
-  geom_histogram(aes(x= Formation, fill = EndzoneScore), position = "fill", binwidth = 5)+
-  labs(x = "Formation Type", y = "Play scored")+
-  ggtitle("Endzone Scores Based on Formation")+
-  scale_fill_brewer("Scored", palette = "YlOrRd")
 
 #Initial Random Forest ----
 RNGkind(sample.kind = "default")
 set.seed(172172) 
+
 
 #I am using an 80/20 split for training and testing, this is pretty standard
 train.idx = sample(x = 1:nrow(fp_cleaned), size = .8*nrow(fp_cleaned))
@@ -246,8 +170,8 @@ set.seed(172172172)
 
 #Build the tree with the training information
 initialTree = rpart(EndzoneScore ~ ., 
-                    data = train.df,
-                    method = 'class') 
+              data = train.df,
+              method = 'class') 
 
 rpart.plot(initialTree)
 
@@ -271,6 +195,7 @@ mtry = c(1:18)
 keeps = data.frame(m = rep(NA, length(mtry)),
                    OOB_error_rate = rep(NA, length(mtry)))
 
+#Create a random forest for each of the possible mtry values
 for(idx in 1:length(mtry)){
   print(paste0("Fitting m = ", mtry[idx]))
   tempforest = randomForest(EndzoneScore ~.,
@@ -281,130 +206,117 @@ for(idx in 1:length(mtry)){
   keeps[idx, "OOB_error_rate"] = mean(predict(tempforest) != train.df$EndzoneScore)
 }
 
+#Plot the OOB Error Rate vs. mtry values
 ggplot(data = keeps) + 
-  geom_line(aes(x = m, y = OOB_error_rate))
+  geom_line(aes(x = m, y = OOB_error_rate))+
+  labs(x = "m (Number of Features Used in Random Forest)", y = "OOB Error Rate") +
+  ggtitle("Plot of OOB Error Rate vs. Number of Fetures Used")
 
-#Using mtry = 12 resulted in the lowest OO
+#Using mtry = 11 resulted in the lowest OOB
 min(keeps$OOB_error_rate)
 
 #TUNED FOREST
 tunedFpForest = randomForest(EndzoneScore ~.,
                              data = train.df,
-                             ntree = 10000,
-                             mtry = 12,
+                             ntree = 1000,
+                             mtry = 11,
                              importance = TRUE)
 
 #Examine the new forest
 tunedFpForest
 
 
-#ROC CURVE
+#Plot ROC Curve
 pi_hat = predict(tunedFpForest, test.df, type = "prob")[,"Score"]
 rocCurve = roc(response = test.df$EndzoneScore,
                predictor = pi_hat,
                levels = c("No Score", "Score"))
-plot(rocCurve, print.thres = "best", print.auc = TRUE)
-#AUC = .997
-#Pi star = .110
-#Sensitivity = .985
-#Specificity = .982                   
-
-fp_cleaned$score_bin = ifelse(fp_cleaned$EndzoneScore == "Score", 1, 0)
-#First model, adding YardLine because it was the #1 variable on variable importance
-m1 = glm(score_bin ~ YardLine, data = fp_cleaned,
-         family =  binomial(link = "logit"))
-AIC(m1) #1940.69
+plot(rocCurve, print.thres = "best", print.auc = TRUE, main = "ROC Curve for Random Forest")
 
 
-#Add SeriesFirstDown
-m2 = glm(score_bin~YardLine + SeriesFirstDown, data = fp_cleaned,
-         family = binomial(link = "logit"))
-AIC(m2) #1331.847
+#Produce an importance plot of the tuned random forest
+varImpPlot(tunedFpForest, type = 1)
+vi = as.data.frame(varImpPlot(tunedFpForest, type = 1))
+vi$Variable <- rownames(vi)
+ggplot(data = vi) + 
+  geom_bar(fill = "#99d8c9", aes(x = reorder(Variable,MeanDecreaseAccuracy), weight = MeanDecreaseAccuracy), 
+           position ="identity") +
+  coord_flip() + 
+  labs( x = "Variable Name",y = "Importance") + 
+  ggtitle("Variable Importance")
+
+#Evan's Refined Plots----
+#This plot is interesting, extremely skewed to the right.
+#This makes sense though, PI calls can greatly impact a play's success
+fp_cleaned %>%
+  filter(IsPenaltyAccepted == 1) %>%
+  ggplot(aes(x = PenaltyYards)) +
+  geom_histogram(binwidth = 1)
+
+#Attempt to show differences between play types and penalty yards.
+#This is consistent with what I expected to see
+fp_cleaned %>%
+  filter(IsPenaltyAccepted == 1) %>%
+  ggplot(aes(x = PenaltyYards, fill = PlayType)) +
+  geom_histogram(binwidth = 5) + 
+  scale_fill_brewer("Type of\n Play", palette = "BuGn") + 
+  labs(x = "Penalty Yards", y = "Count") +
+  ggtitle("Penalty Yards by Play Type")
+#Add colorblind friendly palette, t
+fp_cleaned %>%
+  filter(IsPenaltyAccepted == 1) %>%
+  ggplot(aes(x = YardLine, y = PenaltyYards, color = factor(PlayType))) + 
+  geom_point()
 
 
-#Add Yards
-m3 = glm(score_bin~YardLine + SeriesFirstDown + Yards, data = fp_cleaned,
-         family = binomial(link = "logit"))
-AIC(m3) #1004.569
+fp_cleaned %>% 
+  filter(IsPenaltyAccepted == 1) %>%
+  ggplot(aes(x = PenaltyYards))
+
+#I thought that this might show the penalty yards that a penalty might
+#Have been worth even though it was not accepted, this is not the case
+#All yards values where the penalty is not accepted are = to 0.
+#This plot is not helpful
+fp_cleaned %>% 
+  filter(IsPenalty == 1 & IsPenaltyAccepted == 0) %>%
+  ggplot(aes(x = PenaltyYards)) + 
+  geom_histogram(binwidth = 5)
 
 
-#Add IsPenalty
-m4 = glm(score_bin~YardLine + SeriesFirstDown + Yards +IsPenalty, data = fp_cleaned,
-         family = binomial(link = "logit"))
-AIC(m4) #936.3637
+#Take a look at the frequency of yds to go
+#This plot is not helpful, the majority of
+#Observations are of plays with ToGo = 10. 
+#This makes sense because when a 1st down is 
+#gained, the next play starts with 10 yds to go
+fp_cleaned %>%
+  ggplot(aes(x = ToGo)) + 
+  geom_histogram(binwidth = 1)
+
+#Look at subsets of plays with < 10 yds to go and > 10 yds to go
+fp_cleaned %>%
+  filter(ToGo < 10) %>%
+  ggplot(aes(x = ToGo)) + 
+  geom_histogram(binwidth = 1)
+
+#Add labels and a different color palette
+fp_cleaned %>%
+  filter(ToGo < 10) %>%
+  ggplot(aes(x = ToGo, fill = PlayType)) + 
+  geom_histogram(binwidth = 1) + 
+  scale_fill_brewer("Type of\n Play", palette = "BuGn") +
+  labs(x = "Distance Until 1st Down", y = "Count") +
+  ggtitle("Distribution of Yards Until 1st Down Gained (< 10 Yards to Go)")
+
+#Add labels and a different color palette
+fp_cleaned %>%
+  filter(ToGo > 10) %>%
+  ggplot(aes(x = ToGo, fill = PlayType)) + 
+  geom_histogram(binwidth = 5) + 
+  scale_fill_brewer("Type of\n Play", palette = "BuGn") +
+  labs(x = "Distance Until 1st Down", y = "Count") +
+  ggtitle("Distribution of Yards Until 1st Down Gained (> 10 Yards to Go)")
 
 
-#Add PenaltyYards
-m5 = glm(score_bin~YardLine + SeriesFirstDown + Yards +IsPenalty +
-           PenaltyYards, data = fp_cleaned,
-         family = binomial(link = "logit"))
-AIC(m5) #937.858 Increase from the past add
 
 
-#Add ToGo
-m6 = glm(score_bin~YardLine + SeriesFirstDown + Yards +IsPenalty +
-           PenaltyYards +ToGo, data = fp_cleaned,
-         family = binomial(link = "logit"))
-AIC(m6) #915.7798 Increase from the past add
 
-
-#Add PassType
-m7 = glm(score_bin~YardLine + SeriesFirstDown + Yards +IsPenalty +
-           PenaltyYards +ToGo+PassType, data = fp_cleaned,
-         family = binomial(link = "logit"))
-AIC(m7) #912.9182 Increase from the past add
-
-
-#Add IsPenaltyAccepted
-m8 = glm(score_bin~YardLine + SeriesFirstDown + Yards +IsPenalty +
-           PenaltyYards +ToGo+PassType+IsPenaltyAccepted, data = fp_cleaned,
-         family = binomial(link = "logit"))
-AIC(m8) #904.94
-summary(m8)
-
-#Add IsNoPlay
-m9 = glm(score_bin~YardLine + SeriesFirstDown + Yards +IsPenalty +
-           PenaltyYards +ToGo+PassType+IsPenaltyAccepted +IsNoPlay, data = fp_cleaned,
-         family = binomial(link = "logit"))
-AIC(m9) #906.9086 #larger than model 8
-
-m10 = glm(score_bin~YardLine + SeriesFirstDown + Yards +IsPenalty +
-           PenaltyYards +ToGo+PassType+IsPenaltyAccepted +IsNoPlay + RushDirection, data = fp_cleaned,
-         family = binomial(link = "logit"))
-AIC(m10) #905.0016 #Both this an the previous are larger than model 8
-
-final_model = glm(score_bin~YardLine + SeriesFirstDown + Yards +IsPenalty +
-                    PenaltyYards +ToGo+PassType+IsPenaltyAccepted, data = fp_cleaned,
-                  family = binomial(link = "logit"))
-summary(final_model)
-
-#overall Model significance
-lr <- -2*(m8$deviance - m8$null.deviance)
-df <- m8$df.null - m8$df.residual
-1-pchisq(lr,df)
-
-anova(m8, test = "Chisq")
-summary(m8)
-
-##Yards plots
-ggplot(data = fp_cleaned)+
-  geom_histogram(aes(x=Yards, fill = EndzoneScore), position = "fill", binwidth = 5)+
-  labs(x = "Total Yards gained on Play", y = "Play scored")+
-  ggtitle("Endzone Scores Based on how Many Yards were Gained")+
-  scale_fill_brewer("Scored", palette = "BuGn")
-
-
-##Is Penalty plots
-ggplot(data = fp_cleaned)+
-  geom_bar(aes(x= IsPenalty, fill = EndzoneScore), position = "fill")+
-  labs(x = "Penalty Happened", y = "Play scored")+
-  ggtitle("Endzone Scores Based on if a Penalty Happened")+
-  scale_fill_brewer("Scored", palette = "BuGn")
-
-fp_cleaned = fp_cleaned %>%
-  mutate(IsPenalty= ifelse(IsPenalty==1, "Penalty", "No Penalty"))
-ggplot(data = fp_cleaned)+
-  geom_bar(aes(x= IsPenalty, fill = PassType),position = "fill")+
-  labs(x = "Penalty Happened", y = "Type of Pass")+
-  ggtitle("Penalties based on what Pass Type the play was")+
-  scale_fill_brewer("Pass Type", palette = "BuGn")
